bcd9f4e7c1f1938d6e62eedcf87f991e0a502a22
Maxim Kliashchou <maxim.kliashchou@rdigital.by>
2021-08-18 13:13:17 +0300

    4 тема задачка

---
+++ src/bel/matsvey/theme/operators/Main.java
@@ -0,0 +1,10 @@
+package bel.matsvey.theme.operators;
+
+import bel.matsvey.theme.operators.entity.Human;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.sayHello();
+    }
+}
---
+++ src/bel/matsvey/theme/operators/entity/Human.java
@@ -0,0 +1,7 @@
+package bel.matsvey.theme.operators.entity;
+
+public class Human {
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+}
